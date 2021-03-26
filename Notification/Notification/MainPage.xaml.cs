363b97f6c5b453504cb82a929bd3b1c0daf39d7d
the huy <thehuy168@hotmail.com>
2021-03-26 16:25:25 +0700

    Add project files.

---
+++ Notification/Notification/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notification
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void SendNotification(object sender, EventArgs e)
+        {
+            DependencyService.Get<INotification>().CreateNotification("Hello", message.Text);
+        }
+
+    }
+}
